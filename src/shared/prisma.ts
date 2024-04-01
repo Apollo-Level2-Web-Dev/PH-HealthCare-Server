18f6595e395003712819cf5dea93f302d3943b40
fahimahammed <fahimahammed.cse@gmail.com>
2024-04-02 00:41:32 +0600

    [add] prisma logging

--- src/shared/prisma.ts
+++ src/shared/prisma.ts
@@ -1,5 +1,46 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log: [
+        {
+            emit: 'event',
+            level: 'query',
+        },
+        {
+            emit: 'event',
+            level: 'error',
+        },
+        {
+            emit: 'event',
+            level: 'info',
+        },
+        {
+            emit: 'event',
+            level: 'warn',
+        },
+    ],
+})
+
+prisma.$on('query', (e) => {
+    console.log("-------------------------------------------")
+    console.log('Query: ' + e.query);
+    console.log("-------------------------------------------")
+    console.log('Params: ' + e.params)
+    console.log("-------------------------------------------")
+    console.log('Duration: ' + e.duration + 'ms')
+    console.log("-------------------------------------------")
+})
+
+// prisma.$on('warn', (e) => {
+//     console.log(e)
+// })
+
+// prisma.$on('info', (e) => {
+//     console.log(e)
+// })
+
+// prisma.$on('error', (e) => {
+//     console.log(e)
+// })
 
 export default prisma;
